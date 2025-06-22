bba7461e5191677bba0c8b9ae37b5f69c68b8a17
Ravivardhan <ravivardhanbtechcse@gmail.com>
2025-06-22 23:03:01 +0530

    added CodeEditor&ChangedTheme

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,6 +1,10 @@
 var boilerplate = `<!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="templates/css/sheet.css">
   <title>My HTML Page</title>
   <style>
@@ -10,11 +14,16 @@ var boilerplate = `<!DOCTYPE html>
 <body>
   <h1>Hello, world!</h1>
   <p>This is a basic HTML boilerplate.</p>
+  <div class="container mt-5">
+    <h1 class="text-center text-primary">Welcome to My Page</h1>
+    <button class="btn btn-success">Click Me</button>
+  </div>
 </body>
 </html>`;
 
 var btn=document.getElementById("render")
 var content=document.querySelector("textarea");
+
 var iframe=document.querySelector("iframe");
 content.value=boilerplate;
 render();
@@ -44,3 +53,22 @@ async function exportToPDF() {
     pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
     pdf.save('iframe-content.pdf'); 
   }
+
+  window.onload = function () {
+    const textarea = document.getElementById("codeEditor");
+      const editor = CodeMirror.fromTextArea(textarea, {
+      mode: "htmlmixed",
+      theme: "material",
+      lineNumbers: true,
+      lineWrapping: true
+    });
+  
+    // Render content live into iframe
+    editor.on("change", () => {
+      const iframe = document.querySelector("iframe");
+      iframe.srcdoc = editor.getValue();
+    });
+  
+ 
+  };
+  
